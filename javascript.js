4c7312d1c84d906820f9e1021fd1bad550d5f065
renny5120 <renny5120@gmail.com>
2024-01-10 09:28:33 -0800

    clean up box creation

--- javascript.js
+++ javascript.js
@@ -1,18 +1,27 @@
 let size = 16;
 const container = document.querySelector('.container');
-for (let i = 0; i < size; i++){
-    // Creates row
-    const row = document.createElement('div');
-    row.classList.add('row');
-    // Adds boxes to row
-    for (let j = 0; j < size; j++){
-        const box = document.createElement('div');
-        box.classList.add('box');
-        row.appendChild(box);
-    }
 
-    // Adds row to container
-    container.appendChild(row);
+function createBoxes(){
+    for (let i = 0; i < size; i++){
+        // Creates row
+        const row = document.createElement('div');
+        row.classList.add('row');
+        // Adds boxes to row
+        for (let j = 0; j < size; j++){
+            const box = document.createElement('div');
+            box.classList.add('box');
+            row.appendChild(box);
+        }
+    
+        // Adds row to container
+        container.appendChild(row);
+    }
+}
+
+function deleteBoxes(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
 }
 
 container.addEventListener('mouseover', (e) => {
@@ -29,7 +38,5 @@ container.addEventListener('mouseover', (e) => {
     }
 });
 
-// removes children
-// while(container.firstChild){
-//     container.removeChild(container.firstChild);
-// }
+createBoxes();
+
